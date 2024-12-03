36d584df2997e3ff3a2571b5c468f968b4f5c435
joss0502 <brjoossjara@gmail.com>
2024-12-03 00:01:14 -0600

    feat: :art:

--- djangotutorial/mysite/App/urls.py
+++ djangotutorial/mysite/App/urls.py
@@ -6,5 +6,11 @@ from . import views
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('', views.grafica_view, name='grafica_view'),
+ 
 
+    path('graficas/lineas/', views.grafica_line_view, name='grafica_lineas'),
+    path('graficas/pastel/', views.grafica_pastel_view, name='grafica_pastel'),
+    path('graficas/dispersion/', views.grafica_dispersion_view, name='grafica_dispersion'),
+    path('graficas/histograma/', views.grafica_histograma_view, name='grafica_histograma'),
+    path('graficas/boxplot/', views.grafica_boxplot_view, name='grafica_boxplot'),
 ]
