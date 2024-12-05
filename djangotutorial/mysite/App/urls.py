8e8451a03df5f7182da0d1c3facdcd185bd8b591
joss0502 <brjoossjara@gmail.com>
2024-12-05 01:00:43 -0600

    Update urls.py

--- djangotutorial/mysite/App/urls.py
+++ djangotutorial/mysite/App/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/',admin.site.urls),
-    path('graficas/', views.grafica_view, name='grafica_view'),
- 
-
+    path('', views.grafica_view, name='grafica_view'),
     path('graficas/line/', views.grafica_line_view, name='grafica_line'),
     path('graficas/pastel/', views.grafica_pastel_view, name='grafica_pastel'),
     path('graficas/dispersion/', views.grafica_dispersion_view, name='grafica_dispersion'),
